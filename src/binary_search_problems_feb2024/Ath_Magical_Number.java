bde6fda4a635165916571cd23ed8da2392d5fe87
AswinBarath <aswin2001barath@gmail.com>
2024-03-02 13:03:28 +0530

    Ath Magical Number Problem Description

--- src/binary_search_problems_feb2024/Ath_Magical_Number.java
+++ src/binary_search_problems_feb2024/Ath_Magical_Number.java
@@ -24,3 +24,69 @@ public class Ath_Magical_Number {
         return (int) (ans % mod);
     }
 }
+
+/*
+ * Problem Description
+ * You are given three positive integers, A, B, and C.
+ * 
+ * Any positive integer is magical if divisible by either B or C.
+ * 
+ * Return the Ath smallest magical number. Since the answer may be very large,
+ * return modulo 109 + 7.
+ * 
+ * Note: Ensure to prevent integer overflow while calculating.
+ * 
+ * 
+ * 
+ * Problem Constraints
+ * 1 <= A <= 109
+ * 
+ * 2 <= B, C <= 40000
+ * 
+ * 
+ * 
+ * Input Format
+ * The first argument given is an integer A.
+ * 
+ * The second argument given is an integer B.
+ * 
+ * The third argument given is an integer C.
+ * 
+ * 
+ * 
+ * Output Format
+ * Return the Ath smallest magical number. Since the answer may be very large,
+ * return modulo 109 + 7.
+ * 
+ * 
+ * 
+ * Example Input
+ * Input 1:
+ * 
+ * A = 1
+ * B = 2
+ * C = 3
+ * Input 2:
+ * 
+ * A = 4
+ * B = 2
+ * C = 3
+ * 
+ * 
+ * Example Output
+ * Output 1:
+ * 
+ * 2
+ * Output 2:
+ * 
+ * 6
+ * 
+ * 
+ * Example Explanation
+ * Explanation 1:
+ * 
+ * 1st magical number is 2.
+ * Explanation 2:
+ * 
+ * First four magical numbers are 2, 3, 4, 6 so the 4th magical number is 6.
+ */
